6c535abce399dd0df10aa91c54c715fd5f94f728
NarcissusNull <NarcissusNull@outlook.com>
2021-04-10 22:25:40 +0800

    add search component

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../search.service';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css'],
+})
+export class SearchComponent implements OnInit {
+  constructor(private searchService: SearchService) {}
+
+  ngOnInit(): void {}
+
+  getRecommand(): string {
+    return this.searchService.getRecommand();
+  }
+}
