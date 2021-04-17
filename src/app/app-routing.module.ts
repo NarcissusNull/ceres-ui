3e493c4be135083065501f9c1a6f91201b198c99
NarcissusNull <NarcissusNull@outlook.com>
2021-04-17 14:28:40 +0800

    add search page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-const routes: Routes = [{ path: 'home', component: HomePageComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'search', component: SearchPageComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
